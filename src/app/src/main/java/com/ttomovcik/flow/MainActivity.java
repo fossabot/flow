0bf713f12ee58317ec842ab8f29f5ca60762ee70
ttomovcik <tomovcik.tomas@gmail.com>
2019-02-22 17:25:43 +0100

    [CORE] [UI] Nuked menu and icons

--- src/app/src/main/java/com/ttomovcik/flow/MainActivity.java
+++ src/app/src/main/java/com/ttomovcik/flow/MainActivity.java
@@ -21,15 +21,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                /*
+                case R.id.:
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.:
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.:
                     return true;
+                    */
             }
             return false;
         }
@@ -40,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
