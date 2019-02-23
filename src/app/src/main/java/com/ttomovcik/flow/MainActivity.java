c7787fe15bd47c689cae61f33aaea2b98bb1cda4
ttomovcik <tomovcik.tomas@gmail.com>
2019-02-23 19:48:57 +0100

    [CORE] [UI] Added fragments with titles, added fonts and dimensions

--- src/app/src/main/java/com/ttomovcik/flow/MainActivity.java
+++ src/app/src/main/java/com/ttomovcik/flow/MainActivity.java
@@ -7,6 +7,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +19,32 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.menu_timeline:
+                    Fragment flowFragment = new Flow();
+                    FragmentTransaction fragmentTransaction_flow = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction_flow.replace(R.id.fragment_container, flowFragment);
+                    fragmentTransaction_flow.addToBackStack(null);
+                    fragmentTransaction_flow.commit();
                     return true;
                 case R.id.menu_time_machine:
+                    Fragment timeMachineFragment = new TimeMachine();
+                    FragmentTransaction fragmentTransaction_timeMachine = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction_timeMachine.replace(R.id.fragment_container, timeMachineFragment);
+                    fragmentTransaction_timeMachine.addToBackStack(null);
+                    fragmentTransaction_timeMachine.commit();
                     return true;
                 case R.id.menu_tools:
+                    Fragment toolsFragment = new Tools();
+                    FragmentTransaction fragmentTransaction_tools = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction_tools.replace(R.id.fragment_container, toolsFragment);
+                    fragmentTransaction_tools.addToBackStack(null);
+                    fragmentTransaction_tools.commit();
                     return true;
                 case R.id.menu_profile:
+                    Fragment profileFragment = new Profile();
+                    FragmentTransaction fragmentTransaction_profile = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction_profile.replace(R.id.fragment_container, profileFragment);
+                    fragmentTransaction_profile.addToBackStack(null);
+                    fragmentTransaction_profile.commit();
                     return true;
             }
             return false;
@@ -36,6 +58,13 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // Load the Flow fragment on startup
+        Fragment flowFragment = new Flow();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, flowFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
 }
---
+++ src/app/src/main/java/com/ttomovcik/flow/Tools.java
@@ -0,0 +1,31 @@
+package com.ttomovcik.flow;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Tools extends Fragment {
+
+
+    public Tools() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tools, container, false);
+    }
+
+}
