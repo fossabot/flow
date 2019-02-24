84757b660ad35e678eeaf04b38e01b7e373f82a1
ttomovcik <tomovcik.tomas@gmail.com>
2019-02-24 18:34:01 +0100

    [CORE] Added switchFragment() function

--- src/app/src/main/java/com/ttomovcik/flow/MainActivity.java
+++ src/app/src/main/java/com/ttomovcik/flow/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ttomovcik.flow;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -9,6 +10,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+
+import static com.ttomovcik.flow.R.id.fragment_container;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,32 +22,16 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.menu_timeline:
-                    Fragment flowFragment = new Flow();
-                    FragmentTransaction fragmentTransaction_flow = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction_flow.replace(R.id.fragment_container, flowFragment);
-                    fragmentTransaction_flow.addToBackStack(null);
-                    fragmentTransaction_flow.commit();
+                    switchFragment(new Flow());
                     return true;
                 case R.id.menu_time_machine:
-                    Fragment timeMachineFragment = new TimeMachine();
-                    FragmentTransaction fragmentTransaction_timeMachine = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction_timeMachine.replace(R.id.fragment_container, timeMachineFragment);
-                    fragmentTransaction_timeMachine.addToBackStack(null);
-                    fragmentTransaction_timeMachine.commit();
+                    switchFragment(new TimeMachine());
                     return true;
                 case R.id.menu_tools:
-                    Fragment toolsFragment = new Tools();
-                    FragmentTransaction fragmentTransaction_tools = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction_tools.replace(R.id.fragment_container, toolsFragment);
-                    fragmentTransaction_tools.addToBackStack(null);
-                    fragmentTransaction_tools.commit();
+                    switchFragment(new Tools());
                     return true;
                 case R.id.menu_profile:
-                    Fragment profileFragment = new Profile();
-                    FragmentTransaction fragmentTransaction_profile = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction_profile.replace(R.id.fragment_container, profileFragment);
-                    fragmentTransaction_profile.addToBackStack(null);
-                    fragmentTransaction_profile.commit();
+                    switchFragment(new Profile());
                     return true;
             }
             return false;
@@ -60,11 +47,15 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         // Load the Flow fragment on startup
-        Fragment flowFragment = new Flow();
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.fragment_container, flowFragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
+        Log.d("onCreate","Initial fragment switch should happen soon");
+        switchFragment(new Flow());
     }
 
+    private void switchFragment (Fragment fragmentName) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(fragment_container, fragmentName);
+        fragmentTransaction.addToBackStack(null);
+        Log.d("switchFragment","Switching to: "+ fragmentName);
+        fragmentTransaction.commit();
+    }
 }
