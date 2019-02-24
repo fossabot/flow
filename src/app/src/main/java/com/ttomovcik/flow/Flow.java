b7788581a54bd7509980569d25788a0f6da9ac1e
ttomovcik <tomovcik.tomas@gmail.com>
2019-02-24 19:06:54 +0100

    [CORE] [UI] Added circularImageView and container in Flow fragment

--- src/app/src/main/java/com/ttomovcik/flow/Flow.java
+++ src/app/src/main/java/com/ttomovcik/flow/Flow.java
@@ -3,28 +3,23 @@ package com.ttomovcik.flow;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class Flow extends Fragment {
 
-
-    public Flow() {
-        // Required empty public constructor
-    }
-
+    public Flow(){}
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        Log.d("Flow","Well, hello there!");
         return inflater.inflate(R.layout.fragment_flow, container, false);
     }
 
